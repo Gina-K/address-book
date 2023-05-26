525648d97540ca2a526ce3eccd357edb763a37ef
Gina-K <e.kurmeneva@gmail.com>
2023-05-26 13:51:20 +0200

    add buttons

--- src/components/ContactCard.tsx
+++ src/components/ContactCard.tsx
@@ -14,26 +14,38 @@ export const ContactCard = ({contact, countries}: Props) => {
   return (
     <>
       <div>
-        <label htmlFor="firstName" style={{visibility}}>First Name: <span aria-label="required">*</span></label>
-        <input id="firstName" type="text" name="firstName" value={firstName} required disabled={!isEditing}/>
-      </div>
+        <div>
+          <label htmlFor="firstName" style={{visibility}}>First Name: <span aria-label="required">*</span></label>
+          <input id="firstName" type="text" name="firstName" value={firstName} required disabled={!isEditing} />
+        </div>
 
-      <div>
-        <label htmlFor="lastName" style={{visibility}}>Last Name: <span aria-label="required">*</span></label>
-        <input id="lastName" type="text" name="lastName" value={lastName} required disabled={!isEditing}/>
-      </div>
+        <div>
+          <label htmlFor="lastName" style={{visibility}}>Last Name: <span aria-label="required">*</span></label>
+          <input id="lastName" type="text" name="lastName" value={lastName} required disabled={!isEditing} />
+        </div>
 
-      <div>
-        <label htmlFor="email" style={{visibility}}>Email: <span aria-label="required">*</span></label>
-        <input id="email" type="text" name="email" value={email} required disabled={!isEditing}/>
-      </div>
+        <div>
+          <label htmlFor="email" style={{visibility}}>Email: <span aria-label="required">*</span></label>
+          <input id="email" type="text" name="email" value={email} required disabled={!isEditing} />
+        </div>
 
-      <div>
-        <label htmlFor="country" style={{visibility}}>Country: <span aria-label="required">*</span></label>
-        <select id="country" name="country" value={country} required disabled={!isEditing}>
-          <option value="">Please choose a country</option>
-          {countries.map((country: string) => <option value={country}>{country}</option>)}
-        </select>
+        <div>
+          <label htmlFor="country" style={{visibility}}>Country: <span aria-label="required">*</span></label>
+          <select id="country" name="country" value={country} required disabled={!isEditing}>
+            <option value="">Please choose a country</option>
+            {countries.map((country: string) => <option value={country}>{country}</option>)}
+          </select>
+        </div>
+
+        <div>
+          <button type="button">Delete</button>
+          {isEditing ?
+            <>
+              <button type="button">Cancel</button>
+              <button type="button">Save</button>
+            </>
+            : <button type="button">Edit</button>}
+        </div>
       </div>
     </>
   );
