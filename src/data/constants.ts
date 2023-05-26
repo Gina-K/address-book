9af19e740263e9f2c7bc0d4dae37563ce982052c
Gina-K <e.kurmeneva@gmail.com>
2023-05-26 12:52:28 +0200

    add placeholder contacts list

---
+++ src/data/constants.ts
@@ -0,0 +1,39 @@
+import {Contact} from './types.ts';
+
+export const PLACEHOLDER_CONTACTS: Contact[] = [
+  {
+    id: '1',
+    firstName: 'Eva',
+    lastName: 'Stone',
+    email: 'eva.stone@example.com',
+    country: 'Spain',
+  },
+  {
+    id: '2',
+    firstName: 'Dustin',
+    lastName: 'Scott',
+    email: 'dustin.scott@example.com',
+    country: 'Australia',
+  },
+  {
+    id: '3',
+    firstName: 'Johnny',
+    lastName: 'Mccoy',
+    email: 'johnny.mccoy@example.com',
+    country: 'India',
+  },
+  {
+    id: '4',
+    firstName: 'Clayton',
+    lastName: 'Jacobs',
+    email: 'clayton.jacobs@example.com',
+    country: 'Egypt',
+  },
+  {
+    id: '5',
+    firstName: 'Felicia',
+    lastName: 'Webb',
+    email: 'felicia.webb@example.com',
+    country: 'Malta',
+  },
+];
