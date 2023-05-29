641d363fc2c07e44270862a697bf677d93ffc1e4
Gina-K <e.kurmeneva@gmail.com>
2023-05-29 14:39:17 +0200

    add styles for contact viewer page

--- src/components/ContactsList/ContactListItem.tsx
+++ src/components/ContactsList/ContactListItem.tsx
@@ -1,5 +1,7 @@
 import type {Contact} from '../../data/types.ts';
 
+import '../../styles/ContactsList/ContactListItem.css';
+
 type Props = {
   contact: Contact;
 }
@@ -8,21 +10,21 @@ export const ContactListItem = ({contact}: Props) => {
   const {firstName, lastName, email, country} = contact;
 
   return (
-    <>
-      <div>
-        <span>Name: </span>
-        <span>{`${firstName} ${lastName}`}</span>
+    <div className="contact">
+      <div className="contact__parameter">
+        <span className="contact__parameter__name">Name: </span>
+        <span className="contact__parameter__value">{`${firstName} ${lastName}`}</span>
       </div>
 
-      <div>
-        <span>E-mail: </span>
-        <span>{email}</span>
+      <div className="contact__parameter">
+        <span className="contact__parameter__name">E-mail: </span>
+        <span className="contact__parameter__value">{email}</span>
       </div>
 
-      <div>
-        <span>Country: </span>
-        <span>{country}</span>
+      <div className="contact__parameter">
+        <span className="contact__parameter__name">Country: </span>
+        <span className="contact__parameter__value">{country}</span>
       </div>
-    </>
+    </div>
   );
 };
--- src/components/ContactsList/ContactsList.tsx
+++ src/components/ContactsList/ContactsList.tsx
@@ -2,16 +2,24 @@ import {Contact} from '../../data/types.ts';
 import {useContacts} from '../../hooks/useContacts.ts';
 import {ContactListItem} from './ContactListItem.tsx';
 
+import '../../styles/ContactsList/ContactsList.css';
+
 export const ContactsList = () => {
   const contacts = useContacts();
 
   return (
-    <>
-      {contacts.map((contact: Contact) => (
-        <li key={contact.id}>
-          <ContactListItem contact={contact} />
-        </li>
-      ))}
-    </>
+    <div className="contacts-list">
+      <div className="list-items__wide-divider"></div>
+
+      <ul className="list-items__container">
+        {contacts.map((contact: Contact) => (
+          <li key={contact.id} className="list-items__item">
+            <ContactListItem contact={contact} />
+          </li>
+        ))}
+      </ul>
+
+      <div className="list-items__wide-divider"></div>
+    </div>
   );
 };
