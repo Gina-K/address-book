91e9834505524060173869f0eb91279d3bc41b08
Gina-K <e.kurmeneva@gmail.com>
2023-05-29 18:57:53 +0200

    style navigation

--- src/views/ContactsManager.tsx
+++ src/views/ContactsManager.tsx
@@ -5,7 +5,7 @@ export const ContactsManager = () => {
   return (
     <div className="page-container">
       <header className="header">
-        <h1 className="page-title">Add, delete, and edit contacts</h1>
+        <h1 className="page-title">Add, delete, edit contacts</h1>
         <Link to="contacts-list" className="navigation">Alternative View</Link>
       </header>
       <ContactCardsContainer />
