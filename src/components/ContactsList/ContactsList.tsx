9cae252c9c84083dd410c3ff298ffb6bb7b2d47f
Gina-K <e.kurmeneva@gmail.com>
2023-05-30 19:15:12 +0200

    sort contacts by las name in contacts viewer

--- src/components/ContactsList/ContactsList.tsx
+++ src/components/ContactsList/ContactsList.tsx
@@ -1,3 +1,5 @@
+import {useMemo} from 'react';
+
 import type {Contact} from '../../data/types.ts';
 import {useContacts} from '../../hooks/useContacts.ts';
 import {ContactListItem} from './ContactListItem.tsx';
@@ -6,13 +8,16 @@ import '../../styles/ContactsList/ContactsList.css';
 
 export const ContactsList = () => {
   const contacts = useContacts();
+  const sortedContacts = useMemo(() => {
+    return contacts.sort((a: Contact, b: Contact) => a.lastName.localeCompare(b.lastName));
+  }, [contacts]);
 
   return (
     <div className="contacts-list">
       <div className="list-items__wide-divider"></div>
 
       <ul className="list-items__container">
-        {contacts.map((contact: Contact) => (
+        {sortedContacts.map((contact: Contact) => (
           <li key={contact.id} className="list-items__item">
             <ContactListItem contact={contact} />
           </li>
